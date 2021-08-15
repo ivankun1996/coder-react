f2b60c1fa35e4d573a6dd7b1e709f8162cde5e2e
ivankun1996 <ivanrapisarda7@gmail.com>
2021-08-14 23:17:24 -0300

    NavBar e ItemListContainer

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavBar.css'
+
+export const NavBar = () => {
+
+
+
+    return (
+
+        <header  className="encabezado">
+            <h1>LOGO</h1>
+            <hr/>
+            <a href="">Enlace 1</a>
+            <a href="">Enlace 2</a>
+            <a href="">Enlace 3</a>
+        </header>
+    )
+}
+
